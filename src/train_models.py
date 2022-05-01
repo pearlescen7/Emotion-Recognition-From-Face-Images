33c7c5a52325ba7cdb5d0d9af96b2c2be6fdc217
Abdulkadir Pazar <pazara18@itu.edu.tr>
2022-05-02 02:10:44 +0300

    Validation loop fixes and accuracy per category added

--- src/train_models.py
+++ src/train_models.py
@@ -19,7 +19,7 @@ test_size = (len(kdef_dataset) - train_size) // 2
 val_size = len(kdef_dataset) - train_size - test_size
 train_set, test_set, val_set = random_split(kdef_dataset, [train_size, test_size, val_size])
 
-batch_size = 4
+batch_size = 32
 
 train_loader = KDEFDataLoader(train_set, batch_size=batch_size)
 test_loader = KDEFDataLoader(test_set, batch_size=batch_size)
@@ -32,13 +32,14 @@ print(f"{len(val_loader.dataset)} validation data loaded.")
 EPOCHS = 80
 LR = 0.001
 MOMENTUM = 0.9
-VALID_PER_EPOCH = 4
-SAVE_PATH = "../models/VGG/vgg11_bn_KDEF.pt"
+VALID_LOSS_PER_EPOCH = 4
+TRAIN_LOSS_PER_EPOCH = 8
+SAVE_PATH = "../models/VGG/vgg19_bn_KDEF.pt"
 
 def generate_save_path(idx, save_path=SAVE_PATH):
     return save_path.removesuffix(".pt") + f"_{idx}.pt"
 
-model = M.vgg11_bn(num_classes=kdef_dataset.num_classes).cuda()
+model = M.vgg19_bn(num_classes=kdef_dataset.num_classes).cuda()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM, weight_decay=0.0005)
@@ -60,12 +61,12 @@ for epoch in range(EPOCHS):
 
         running_loss += loss.item()
 
-        if i % 25 == 24:
-            print(f"[EPOCH {epoch+1}, BATCH {i+1}, TOTAL {epoch*train_size + i*batch_size}] training loss: {running_loss/25:.8f}")
-            train_losses.append(running_loss)
+        if i % int(len(train_loader)/TRAIN_LOSS_PER_EPOCH) == 0 and i!=0:
+            print(f"[EPOCH {epoch+1}, BATCH {i+1}, TOTAL {epoch*train_size + i*batch_size}] training loss: {running_loss/TRAIN_LOSS_PER_EPOCH:.8f}")
+            train_losses.append((running_loss, epoch))
             running_loss = 0.0
 
-        if i % int(len(train_loader.dataset)/VALID_PER_EPOCH) == 0 and i!=0:
+        if i % int(len(train_loader)/VALID_LOSS_PER_EPOCH) == 0 and i!=0:
             model.eval()
             valid_loss = 0.0
             with torch.no_grad():
@@ -78,7 +79,7 @@ for epoch in range(EPOCHS):
             
                     valid_loss += loss.item()
             valid_loss /= len(val_loader)
-            validation_losses.append(valid_loss)
+            validation_losses.append((valid_loss, epoch))
             print(f"[EPOCH {epoch+1}, BATCH {i+1}, TOTAL {epoch*train_size + i*batch_size}] validation loss: {valid_loss:.8f}")
             model.train()
 
@@ -87,9 +88,11 @@ for epoch in range(EPOCHS):
 
 torch.save(model.state_dict(), SAVE_PATH)
 
-plt.plot(train_losses)
-plt.plot(validation_losses)
-plt.title('VGG11 (Batch Normalized) Loss Plot')
+train_loss, train_epoch = tuple([list(tup) for tup in zip(*train_losses)])
+valid_loss, valid_epoch = tuple([list(tup) for tup in zip(*validation_losses)])
+plt.plot(train_epoch, train_loss)
+plt.plot(valid_epoch, valid_loss)
+plt.title('VGG19 (Batch Normalized) Loss Plot')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(["Train", "Validation"], loc='upper right')
@@ -97,6 +100,21 @@ plt.savefig("../models/losses.png")
 
 correct = 0
 total = 0
+
+categories = {"afraid","angry","disgusted","happy","neutral","sad","surprised"}
+category_correct = {category : 0 for category in categories}
+category_total = {category : 0 for category in categories}
+
+label2category = \
+{
+    0 : "afraid"   ,
+    1 : "angry"    , 
+    2 : "disgusted", 
+    3 : "happy"    , 
+    4 : "neutral"  , 
+    5 : "sad"      , 
+    6 : "surprised"
+}
 
 model.eval()
 with torch.no_grad():
@@ -109,4 +127,12 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
+        for prediction, label in zip(predicted, labels):
+            if(prediction == label):
+                category_correct[label2category[label.item()]] += 1
+            category_total[label2category[label.item()]] += 1
+
 print(f"Accuracy: {100*correct/total}%")
+
+for category in category_correct:
+    print(f"Accuracy for {category}: {100*category_correct[category]/category_total[category]}%")
