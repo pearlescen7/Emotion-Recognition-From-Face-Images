1ce96e5d8fad600e6f5ced86460d77e437375464
Abdulkadir Pazar <pazara18@itu.edu.tr>
2022-05-27 12:10:27 +0300

    modified filepaths on test.py

--- src/test.py
+++ src/test.py
@@ -12,25 +12,25 @@ from vgg_pytorch import VGG
 
 kdef_num_classes = 7
 
-# model_path = "../models/EfficientNet/EfficientNet_b7_pretrained_KDEF.pt"
-# model = EfficientNet.from_pretrained('efficientnet-b7')
-# model._fc= torch.nn.Linear(in_features=model._fc.in_features, out_features=kdef_num_classes, bias=True)
-# model.load_state_dict(torch.load(model_path))
+model_path = "../models/EfficientNet/EfficientNet_b7_pretrained_FER_batch8_weighted.pt"
+model = EfficientNet.from_pretrained('efficientnet-b7')
+model._fc= torch.nn.Linear(in_features=model._fc.in_features, out_features=kdef_num_classes, bias=True)
+model.load_state_dict(torch.load(model_path))
 
-# model_path = "../models/VGG/vgg19_bn_pretrained_KDEF.pt"
+# model_path = "../models/VGG/vgg19_bn_pretrained_FER_batch8_weighted.pt"
 # model = VGG.from_pretrained('vgg19_bn', num_classes=kdef_num_classes)
 # model.load_state_dict(torch.load(model_path))
 
-model_path = "../models/VGG/vgg19_bn_KDEF.pt"
-model = M.vgg19_bn(num_classes=kdef_num_classes)
-model.load_state_dict(torch.load(model_path))
+# model_path = "../models/VGG/vgg19_bn_KDEF.pt"
+# model = M.vgg19_bn(num_classes=kdef_num_classes)
+# model.load_state_dict(torch.load(model_path))
 
 model.eval()
 img_transforms = transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-        #                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
         ])
 
 idx2emote_and_color = \
@@ -71,7 +71,7 @@ idx2emote_and_color = \
 face_detector = cv2.CascadeClassifier("../data/haarcascades/haarcascade_frontalface_alt.xml")
 fnames = [("happy-black-guy.jpg", "happy"), ("sad-man.jpg", "sad"), ("happy-furkan.jpg", "happy"), ("angry.png", "angry"), 
         ("happy-woman.jpg", "happy"), ("lena.png", "neutral"), ("surprised-man.jpg", "surprised")]
-
+print(model_path.split('/')[-1])
 for fname in fnames:
     frame = cv2.imread(fname[0])
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
